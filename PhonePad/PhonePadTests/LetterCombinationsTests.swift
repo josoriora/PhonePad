96b5d299eb4cc414e5a6a48b775995bd812216be
Julián Osorio <julian.osorior@gmail.com>
2021-08-28 20:11:29 -0500

    - First iteration with single digit.

--- PhonePad/PhonePadTests/LetterCombinationsTests.swift
+++ PhonePad/PhonePadTests/LetterCombinationsTests.swift
@@ -17,15 +17,31 @@ class LetterCombinationsTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-    func test2input() {
+    func testOneDigitinput() {
         let letterCombinations = LetterCombinations()
         
-        XCTAssertEqual(letterCombinations.wordListFrom(numbers: [2]), ["A", "B", "C"])
+        XCTAssertEqual(letterCombinations.wordListFrom(numbers: [0]), [String]())
+        XCTAssertEqual(letterCombinations.wordListFrom(numbers: [1]), [String]())
+        XCTAssertEqual(letterCombinations.wordListFrom(numbers: [2]), ["a", "b", "c"])
+        XCTAssertEqual(letterCombinations.wordListFrom(numbers: [3]), ["d", "e", "f"])
+        XCTAssertEqual(letterCombinations.wordListFrom(numbers: [4]), ["g", "h", "i"])
+        XCTAssertEqual(letterCombinations.wordListFrom(numbers: [5]), ["j", "k", "l"])
+        XCTAssertEqual(letterCombinations.wordListFrom(numbers: [6]), ["m", "n", "o"])
+        XCTAssertEqual(letterCombinations.wordListFrom(numbers: [7]), ["p", "r", "s"])
+        XCTAssertEqual(letterCombinations.wordListFrom(numbers: [8]), ["t", "u", "v"])
+        XCTAssertEqual(letterCombinations.wordListFrom(numbers: [9]), ["w", "x", "y"])
     }
 }
 
 class LetterCombinations {
+    
+    let letterMapping = ["0", "1", "abc", "def", "ghi", "jkl", "mno", "prs", "tuv", "wxy"]
+    
     func wordListFrom(numbers: [Int]) -> [String] {
-        return [String]()
+        guard let number = numbers.first, number > 1, number <= 9 else {
+            return [String]()
+        }
+        
+        return letterMapping[numbers.first!].map { String($0) }
     }
 }
