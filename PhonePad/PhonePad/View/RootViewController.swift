d14c15c242f21bc2fb39ffcf9d776cd309b170bc
Julián Osorio <julian.osorior@gmail.com>
2021-08-29 21:19:16 -0500

    - Encapsulate collection view in RotaryDialView.

--- PhonePad/PhonePad/View/RootViewController.swift
+++ PhonePad/PhonePad/View/RootViewController.swift
@@ -8,53 +8,26 @@
 import UIKit
 
 class RootViewController: UIViewController {
-       
-    @IBOutlet weak var collectionView: UICollectionView!
-    
-    enum ReuseIdentifier: String {
-        case item = "ItemCell"
-    }
+
+    @IBOutlet weak var rotaryDialView: RotaryDialView!
+    var phoneNumbers: [PhoneNumber] = PhoneNumberDialListFacade.getList()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "Phone Pad"
-        collectionView!.register(CircleCollectionViewCell.self, forCellWithReuseIdentifier: ReuseIdentifier.item.rawValue)
+        rotaryDialView.dataSource = self
     }
     
-    private var items: [UIColor] = [.random(),
-                                    .random(),
-                                    .random(),
-                                    .random(),
-                                    .random(),
-                                    .random(),
-                                    .random(),
-                                    .random(),
-                                    .random(),
-                                    .random()]
-
 }
 
-extension RootViewController: UICollectionViewDataSource {
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return items.count > 0 ? 1 : 0
+extension RootViewController: RotaryDialDataSource {
+    
+    func numberOfDials() -> Int {
+        return phoneNumbers.count
     }
-
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return items.count
+    
+    func numberAt(indexPath: IndexPath) -> PhoneNumber? {
+        return phoneNumbers[indexPath.row]
     }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ReuseIdentifier.item.rawValue, for: indexPath) as! CircleCollectionViewCell
-        cell.backgroundView?.backgroundColor = items[indexPath.item]
-        cell.titleLabel.text = "\(indexPath.item)"
-        return cell
-    }
-}
-
-private extension UIColor {
-    class func random() -> UIColor {
-        return UIColor(hue: .random(in: 0.0...1.0),
-                       saturation: .random(in: 0.5...1.0),
-                       brightness: .random(in: 0.5...1.0), alpha: 1.0)
-    }
+    
 }
---
+++ PhonePad/PhonePad/View/RotaryDial/RotaryDialView.swift
@@ -0,0 +1,80 @@
+//
+//  RotaryDialView.swift
+//  PhonePad
+//
+//  Created by Julian Andres Osorio Ramirez on 29/08/21.
+//
+
+import UIKit
+
+protocol RotaryDialDataSource: AnyObject {
+    func numberOfDials() -> Int
+    
+    func numberAt(indexPath: IndexPath) -> PhoneNumber?
+}
+
+class RotaryDialView: UIView {
+    var collectionView: UICollectionView!
+    let cellIdentifier = "ItemCell"
+    weak var dataSource: RotaryDialDataSource?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupView()
+    }
+    
+    //initWithCode to init view from xib or storyboard
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+    
+    //common func to init our view
+    private func setupView() {
+        backgroundColor = .red
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        let circleCollectionLayout = CircleCollectionViewLayout()
+        
+        collectionView = UICollectionView(frame: self.bounds, collectionViewLayout: circleCollectionLayout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.dataSource = self
+        collectionView.backgroundColor = UIColor.yellow
+        collectionView.register(CircleCollectionViewCell.self, forCellWithReuseIdentifier: cellIdentifier)
+        
+        self.addSubview(collectionView)
+        
+        let constraints = [collectionView.topAnchor.constraint(equalTo: self.topAnchor),
+                           collectionView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+                           collectionView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+                           collectionView.bottomAnchor.constraint(equalTo: self.bottomAnchor)]
+        
+        NSLayoutConstraint.activate(constraints)
+    }
+}
+
+extension RotaryDialView: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        guard let numberOfDials = self.dataSource?.numberOfDials() else { return 0 }
+        
+        return numberOfDials > 0 ? 1 : 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.dataSource?.numberOfDials() ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard
+            let phoneNumber = self.dataSource?.numberAt(indexPath: indexPath),
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as? CircleCollectionViewCell
+        else {
+            return UICollectionViewCell()
+        }
+        
+        cell.backgroundView?.backgroundColor = .blue
+        cell.titleLabel.text = String(phoneNumber.number)
+        return cell
+    }
+}
